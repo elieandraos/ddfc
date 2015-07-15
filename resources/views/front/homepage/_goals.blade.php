2dc5e0215fcc9d0b37c581d128339d6566be28de
jsaade <jsaade@gmail.com>
2015-07-15 09:59:05 +0300

    home page golas links

--- resources/views/front/homepage/_goals.blade.php
+++ resources/views/front/homepage/_goals.blade.php
@@ -12,25 +12,37 @@
 	<div class="row">
 		<div class="col-sm-12 col-sm-push-1">
 			<div class="row">
+
 				<div class="col-sm-2 goal">
-					<img src="/images/icon-01.png" alt='Health And Rehabilitation' title='Health And Rehabilitation' />
-					<h2 class="heading11">Health And Rehabilitation</h2>
+				    <a href="/posts/goals/category/health">
+                        <img src="/images/icon-01.png" alt='Health And Rehabilitation' title='Health And Rehabilitation' />
+                        <h2 class="heading11">Health And Rehabilitation</h2>
+					</a>
 				</div>
+
 				<div class="col-sm-2 goal">
+				     <a href="/posts/goals/category/education">
 					<img src="/images/icon-02.png" alt='Education' title='Education' />
 					<h2 class="heading11">Education</h2>
+					</a>
 				</div>
 				<div class="col-sm-2 goal">
-					<img src="/images/icon-03.png" alt='Employment' title='Employment' />
-					<h2 class="heading11">Employment</h2>
+				    <a href="/posts/goals/category/employment">
+                        <img src="/images/icon-03.png" alt='Employment' title='Employment' />
+                        <h2 class="heading11">Employment</h2>
+					</a>
 				</div>
 				<div class="col-sm-2 goal">
-					<img src="/images/icon-04.png" alt='Social Protection' title='Social Protection' />
-					<h2 class="heading11">Social protection</h2>
+				    <a href="/posts/goals/category/social-protection">
+                        <img src="/images/icon-04.png" alt='Social Protection' title='Social Protection' />
+                        <h2 class="heading11">Social protection</h2>
+					</a>
 				</div>
 				<div class="col-sm-2 goal">
-					<img src="/images/icon-05.png" alt='Universal Accessibility' title='Universal Accessibility' />
-					<h2 class="heading11">Universal Accessibility</h2>
+				    <a href="/posts/goals/category/universal-accessibility">
+                        <img src="/images/icon-05.png" alt='Universal Accessibility' title='Universal Accessibility' />
+                        <h2 class="heading11">Universal Accessibility</h2>
+					</a>
 				</div>
 			</div>
 		</div>
