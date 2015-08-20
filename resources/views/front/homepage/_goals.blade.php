56995eaf577f0c9883d66f847af5eb59c1aa8ded
Elie Andraos <elie.andraos@E-Andraos.local>
2015-08-20 18:50:33 +0300

    fixed goals link in hp

--- resources/views/front/homepage/_goals.blade.php
+++ resources/views/front/homepage/_goals.blade.php
@@ -14,26 +14,26 @@
 			<div class="row">
 
 				<div class="col-sm-2 goal">
-				    <a href="/posts/goals/category/health">
+				    <a href="/goals/category/quality-health-and-rehabilitation-services">
                         <img src="/images/icon-01.png" alt="{{trans('messages.Health Alt')}}"/>
                         <h2 class="heading11">{{trans('messages.Health')}}</h2>
 					</a>
 				</div>
 
 				<div class="col-sm-2 goal">
-				    <a href="/posts/goals/category/education">
+				    <a href="/posts/goals/category/inclusive-education">
 					<img src="/images/icon-02.png" alt="{{trans('messages.Education Alt')}}" />
 					<h2 class="heading11">{{trans('messages.Education')}}</h2>
 					</a>
 				</div>
 				<div class="col-sm-2 goal">
-				    <a href="/posts/goals/category/employment">
+				    <a href="/posts/goals/category/equal-employment-opportunities">
                         <img src="/images/icon-03.png" alt="{{trans('messages.Employment Alt')}}" />
                         <h2 class="heading11">{{trans('messages.Employment')}}</h2>
 					</a>
 				</div>
 				<div class="col-sm-2 goal">
-				    <a href="/posts/goals/category/social-protection">
+				    <a href="/posts/goals/category/sustainable-social-protection-system">
                         <img src="/images/icon-04.png" alt="{{trans('messages.Social Protection Alt')}}" />
                         <h2 class="heading11">{{trans('messages.Social Protection')}}</h2>
 					</a>
