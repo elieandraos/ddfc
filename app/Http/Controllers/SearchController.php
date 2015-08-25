1fc645ed4655939acc079a998d151cc01a794aab
Elie Andraos <elie.andraos@E-Andraos.local>
2015-08-25 14:11:10 +0300

    fixed search by locales

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\NewsTranslation;
 use App\Models\PostTranslation;
 use Input;
+use Lang;
+use App\Models\Locale;
 
 
 class SearchController extends Controller {
@@ -19,9 +21,11 @@ class SearchController extends Controller {
 	public function index()
 	{
 		$input = Input::all();
+		$locale = Locale::where('language', '=', Lang::getLocale())->first();
+		
 		$query = $input['search'];
-		$newsTranslations  = NewsTranslation::search($query, null, true)->with('news')->get();
-		$postsTranslations = PostTranslation::search($query, null, true)->with('post')->get();
+		$newsTranslations  = NewsTranslation::where('locale_id', '=', $locale->id)->search($query, null, true)->with('news')->get();
+		$postsTranslations = PostTranslation::where('locale_id', '=', $locale->id)->search($query, null, true)->with('post')->get();
 
 		return view('front.search.index', ['newsTranslations' => $newsTranslations, 'postsTranslations' => $postsTranslations]);
 		
