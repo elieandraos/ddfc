b34a3130300265b8a153107cd4fb2bfbe63d392a
jsaade <jsaade@gmail.com>
2015-09-23 16:24:17 +0300

    coming soon css

--- resources/views/front/comingsoon.blade.php
+++ resources/views/front/comingsoon.blade.php
@@ -2,13 +2,16 @@
 <head>
 	<title>My Community</title>
 	<style>
-		body 
-		{
+		body {
 			background-color: #3b5485;
-			background-image: url('/images/comingsoon.jpg');
+			background-image: url("/images/comingsoon.jpg");
 			background-position: center center;
 			background-repeat: no-repeat;
-			background-size: cover;
+			float: left;
+			height: 100%;
+			margin: 0;
+			padding: 0;
+			width: 100%;
 		}
 	</style>
 </head>
