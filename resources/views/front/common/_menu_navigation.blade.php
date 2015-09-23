f55c7bff077411174f985140a7bfacc25ce0e282
jsaade <jsaade@gmail.com>
2015-09-23 16:32:07 +0300

    top menu fix

--- resources/views/front/common/_menu_navigation.blade.php
+++ resources/views/front/common/_menu_navigation.blade.php
@@ -15,7 +15,7 @@
 
         <!-- Mobile Menu -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-navigation" aria-expanded="false">
+            <button type="button"  class="navbar-toggle collapsed nav-float" data-toggle="collapse" data-target="#menu-navigation" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
