07c4f6a55de182fd12f918845f0efea6f343ba8c
Elie Andraos <elie.andraos@E-Andraos.local>
2015-10-02 16:27:26 +0300

    chanegs

--- resources/views/front/news/_related.blade.php
+++ resources/views/front/news/_related.blade.php
@@ -18,7 +18,7 @@
 		<div class='col-sm-7'>
 			<a href="{!! route('news.show', [$related_n->slug]) !!}" title="{!! $related_n->title !!}" >
 				<h3 class="heading9">{!! str_limit($related_n->title,20,$end='...') !!}</h3>
-				<p  class="heading7">{!! str_limit($related_n->excerpt,40,$end='...') !!}</p>
+				<p  class="heading7">{!! str_limit($related_n->excerpt,55,$end='...') !!}</p>
 			</a>
 		</div>
 	</div>
