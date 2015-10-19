ba30d3585725bf8d5cffc07e7c88e60e8d24d449
Elie Andraos <elie.andraos@E-Andraos.local>
2015-10-19 14:05:08 +0300

    fixes

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8"> 
  {!! MetaTag::generate() !!}
+ <link rel="shortcut icon" href="{{ asset('logo.ico') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <meta http-equiv="Content-Language" content="ar,en" />
 
