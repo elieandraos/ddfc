5ffafb2e90f3b45a8638d1dd72a432e7e6597819
Elie Andraos <elie.andraos@E-Andraos.local>
2015-10-20 15:05:54 +0300

    fixed search relevance

--- app/Models/PostTranslation.php
+++ app/Models/PostTranslation.php
@@ -10,7 +10,7 @@ class PostTranslation extends Model {
 	protected $searchable = [
         'columns' => [
             'title' => 10,
-            'description' => 1,
+            'description' => 2,
             'excerpt'	=> 10
         ],
         'joins' => [
--- resources/views/front/search/_posts.blade.php
+++ resources/views/front/search/_posts.blade.php
@@ -1,21 +1,23 @@
 @foreach($postsTranslations as $postTranslation)
-<div class="row show-item">
-	<a href="{!! route('posts.show', [$postTranslation->post->postType->slug, $postTranslation->post->slug]) !!}">
-		<div class="col-sm-3">
-			<div class="tint black">
+	@if( (Lang::getLocale() == "en" && $postTranslation->post->is_en == 1) || (Lang::getLocale() == "ar" && $postTranslation->post->is_ar == 1))
+		<div class="row show-item">
+			<a href="{!! route('posts.show', [$postTranslation->post->postType->slug, $postTranslation->post->slug]) !!}">
+				<div class="col-sm-3">
+					<div class="tint black">
 
-				@if($postTranslation->post->getFirstMediaURL( $postTranslation->post->getMediaCollectionName(), 'thumb-large'))
-					<img class="home-news-thumb" src="{!! $postTranslation->post->getFirstMediaURL( $postTranslation->post->getMediaCollectionName(), 'thumb-large') !!}" alt="{!! $postTranslation->post->excerpt !!}" />
-				@else 
-					<img src="/images/noimage.jpg" class="home-news-thumb" alt="{!! $postTranslation->post->excerpt !!}" />
-				@endif
+						@if($postTranslation->post->getFirstMediaURL( $postTranslation->post->getMediaCollectionName(), 'thumb-large'))
+							<img class="home-news-thumb" src="{!! $postTranslation->post->getFirstMediaURL( $postTranslation->post->getMediaCollectionName(), 'thumb-large') !!}" alt="{!! $postTranslation->post->excerpt !!}" />
+						@else 
+							<img src="/images/noimage.jpg" class="home-news-thumb" alt="{!! $postTranslation->post->excerpt !!}" />
+						@endif
 
-			</div>
+					</div>
+				</div>
+				<div class="col-sm-9">
+					<h3 class="heading3">{!! $postTranslation->title !!}</h3>
+					<p class="heading4">{!! $postTranslation->excerpt !!}</p>
+				</div>
+			</a>
 		</div>
-		<div class="col-sm-9">
-			<h3 class="heading3">{!! $postTranslation->title !!}</h3>
-			<p class="heading4">{!! $postTranslation->excerpt !!}</p>
-		</div>
-	</a>
-</div>
+	@endif
 @endforeach
