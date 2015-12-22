c791cd83cc15a12529ff42dd059cc248a355c1c5
Elie Andraos <elie.andraos@h-kilany.quantum.com.lb>
2015-12-22 12:39:43 +0200

    fixes

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8"> 
  {!! MetaTag::generate() !!}
  <link rel="shortcut icon" href="{{ asset('logo.ico') }}">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
   <meta http-equiv="Content-Language" content="ar,en" />
 
     <!-- styles --> 
