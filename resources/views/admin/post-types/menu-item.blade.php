2ac82730b0fce80bc230558f4cb1841d4d6edc27
Elie Andraos <elie.andraos@h-kilany.quantum.com.lb>
2016-01-13 12:45:42 +0200

    fixes

--- resources/views/admin/post-types/menu-item.blade.php
+++ resources/views/admin/post-types/menu-item.blade.php
@@ -1,9 +1,20 @@
 @if($authUser->can('manage-'.$postType->slug) || $authUser->is('superadmin') && $postType)
 <li class="sub-menu">
-    <a href="javascript:void(0);"><i class="fa fa-book"></i><span>{{ $postType->title }}</span><i class="arrow fa fa-angle-right pull-right"></i></a>
+    <a href="javascript:void(0);"><i class="fa fa-book"></i><span>
+    	@if($postType->slug=="goals") Services @else {{ $postType->title }} @endif
+    </span><i class="arrow fa fa-angle-right pull-right"></i>
+	</a>
     <ul>
-        <li><a href="{{ route('admin.posts.list', [$postType->id]) }}"><i class="arrow fa fa-angle-right"></i>List {{ $postType->title }}</a></li>
-        <li><a href="{{ route('admin.posts.create', [$postType->id]) }}"><i class="arrow fa fa-angle-right"></i>Create {{ $postType->title }}</a></li>
+        <li>
+        	<a href="{{ route('admin.posts.list', [$postType->id]) }}"><i class="arrow fa fa-angle-right"></i>
+        		List @if($postType->slug=="goals") Services @else {{ $postType->title }} @endif
+        	</a>
+        </li>
+        <li>
+        	<a href="{{ route('admin.posts.create', [$postType->id]) }}"><i class="arrow fa fa-angle-right"></i>
+        		Create @if($postType->slug=="goals") Service @else {{ $postType->title }} @endif
+        	</a>
+        </li>
     </ul>
 </li>
 @endif
